4e5c4ebd7e1e94deb8653cb65e29e079edb18006
David <david.diez31@free.fr>
2017-01-24 12:22:37 +0100

    last commit

--- index.js
+++ index.js
@@ -22,70 +22,67 @@ $(document).ready(function() {
         return httpRequest;
     };
 
-    // var xhr = getHttpRequest();
-    // xhr.open('GET', 'index.xml', true);
-    // // On envoit un header pour indiquer au serveur que la page est appellée en Ajax
-    // xhr.setRequestHeader('X-Requested-With', 'xmlhttprequest');
-    // // On lance la requête
-    // xhr.send();
-    // xhr.onreadystatechange = function() {
-    //     if (xhr.readyState === 4) {
-    //         if (xhr.status === 200) {
-    //             xhr.responseText // contient le résultat de la page
-    //                 //root
-    //         } else {
-    //             // Le serveur a renvoyé un status d'erreur
-    //             console.log("erreur");
-    //         }
-    //     };
-    // };
-    $.ajax({
-        type: "GET",
-        url: "index.xml",
-        dataType: "xml",
-        success: function(xml) {
-            $(xml).find('repertoire').each(
-                function() {
-                    var client = $(this).find('client').text();
-                    console.log(client);
+    var xhr = getHttpRequest();
+    xhr.open('GET', 'index.xml', true);
+    // On envoit un header pour indiquer au serveur que la page est appellée en Ajax
+    xhr.setRequestHeader('X-Requested-With', 'xmlhttprequest');
+    // On lance la requête
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                xhr.responseText // contient le résultat de la page
+                    //root
 
-
-
-                    var nom = $(this).find('nom').eq().html();
-                    console.log(nom);
-
-                    var prenom = $(this).find('prenom').html();
-                    console.log(prenom);
-                    var age = $(this).find('age').html();
-                    console.log(age);
-                    var profession = $(this).find('profession').html();
-                    console.log(profession);
-                    var telephone = $(this).find('telephone').html();
-                    console.log(telephone);
-                    var email = $(this).find('email').html();
-                    console.log(email);
-
-
-
-                    // $('<div class="items" id="link_' + client + '"></div>').html('<a href="' + prenom + '">' + nom + '</a>').appendTo('.reponse');
-                    $(this).find('desc').each(
-                        function() {
-                            var brief = $(this).find('brief').text();
-                            var long = $(this).find('long').text();
-                            $('<div class="brief"></div>').html(brief).appendTo('#link_' + id);
-                            $('<div class="long"></div>').html(long).appendTo('#link_' + id);
-                        });
-
+                $.ajax({
+                    type: "GET",
+                    url: "index.xml",
+                    dataType: "xml",
+                    success: xmlFunction,
+                    error: function() {
+                        alert("404 Not Found - Oops something went wrong !");
+                    }
                 });
 
+                var value, num, nom, prenom;
+                var tabNum = [];
 
-        }
+                function xmlFunction(xml) {
 
-    });
+                    $(xml).find('client').each(function() {
+                        num = $(this).attr('num');
+                        tabNum.push(num);
+                        nom = $(this).find('nom').text();
+                        prenom = $(this).find('prenom').text();
+                        $(".liste").append($('<option>', { value: num, text: prenom + " " + nom }));
+                    });
 
+                    $('select').change(function() {
+                        value = this.value;
+                        var x = tabNum.indexOf(value);
+                        nom = xml.getElementsByTagName("nom")[x].firstChild.nodeValue;
+                        prenom = xml.getElementsByTagName("prenom")[x].firstChild.nodeValue;
+                        var age = xml.getElementsByTagName("age")[x].firstChild.nodeValue;
+                        var profession = xml.getElementsByTagName("profession")[x].firstChild.nodeValue;
+                        var telephone = xml.getElementsByTagName("telephone")[x].firstChild.nodeValue;
+                        var email = xml.getElementsByTagName("email")[x].firstChild.nodeValue;
 
+                        $(".selectedClient").html(
+                            "Nom : " + nom + "<br>" +
+                            "Prénom : " + prenom + "<br>" +
+                            "Age : " + age + "<br>" +
+                            "Profession : " + profession + "<br>" +
+                            "Tel : " + telephone + "<br>" +
+                            "Mail : " + email);
+                    })
 
+                };
 
-
+            } else {
+                // Le serveur a renvoyé un status d'erreur
+                console.log("erreur");
+            }
+        };
+    };
 
 });
